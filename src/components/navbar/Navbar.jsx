1ffd5b4e262b35c515718cf5bfb5df7240c73f85
alimmondal <italimbd@gmail.com>
2022-10-15 15:57:04 +0600

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">lamabooking</span>
+        <div className="navItem">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
